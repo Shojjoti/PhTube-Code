ee5ad460c5ca443465bf2bc10ae51eac8e61c0d4
Shojjoti <shojjotihhossenbd30@gmail.com>
2024-11-28 16:21:47 +0600

    display category

--- scripts/videos.js
+++ scripts/videos.js
@@ -3,8 +3,8 @@ console.log("videos added")
 const loadCategories=() => {
     //fetch the data
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories") 
-    .then((res)=>res.json())    
-    .then((data)=> console.log(data))
+    .then((res)=> res.json())    
+    .then((data)=> DisplayCategories(data.categories))
     .catch((error) => console.log(error));
 
 }
@@ -12,8 +12,21 @@ const loadCategories=() => {
 
 //create DisplayCategories
 
-const DisplayCategories = (data) => {
-    // add data in html 
-}
+const DisplayCategories = (categories) => {
+    const categoryContainer = document.getElementById("categories") ;
 
+    categories.forEach((item) => {
+        console.log(item);
+
+        //create a button 
+        const button = document.createElement("button");
+        button.classList = "btn" ;
+        button.innerText = item.category ; 
+
+        //add button to category container
+        categoryContainer.append(button);
+        
+    });
+
+} ;
 loadCategories();
